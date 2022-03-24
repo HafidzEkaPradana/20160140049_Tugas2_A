eb5cf5241f47817c245165416189ef402580c643
HafidzEkaPradana <hafidz.eka.2016@ft.umy.ac.id>
2022-03-24 07:37:40 +0700

    Add project files.

---
+++ 20160140049_Tugas2_A/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20160140049_Tugas2_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //memanggil method koneksi
+            new Program().Connection();
+            //memanggil method tampilan
+            new Program().Tampilan();
+            //memanggil method menu
+            new Program().Menu();
+            
+        }
+
+        public void Connection()
+        {
+            
+            //Membuat koneksi dengan database
+            using (SqlConnection con = new SqlConnection("data source=LAPTOP-8MKEQ456; " +
+                "database=Tugas2PABD; Integrated Security=True; User ID=sa;Password=mentepermaib20"))
+            {
+                //membuka database
+                con.Open();
+            }
+        }
+
+        public void Tampilan()
+        {
+            //Untuk tampilan
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("                        Program Penyewaan Toko di MALL                        ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine();
+        }
+
+        public void Menu()
+        {
+
+            //Menu
+            int menu;
+            Console.WriteLine();
+            Console.WriteLine("Menu : ");
+            Console.WriteLine("1. Customer");
+            Console.WriteLine("2. Toko");
+            Console.WriteLine("3. Sewa");
+            Console.WriteLine("");
+            Console.Write("Pilih Menu 1/2/3 : ");
+            menu = Convert.ToInt32(Console.ReadLine());
+            if (menu == 1)
+            {
+                Console.Clear();
+                new Program().Tampilan();
+                new Customer().DisplayCustomer();
+            }
+            else
+            {
+                Console.WriteLine("Tidak ada menu tersebut");
+                Console.WriteLine("Tekan tombol apapun untuk kembali");
+                Console.ReadKey();
+                Console.Clear();
+                new Program().Tampilan();
+                new Program().Menu();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 20160140049_Tugas2_A/Toko.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace _20160140049_Tugas2_A
+{
+    class Toko
+    {
+        public void DisplayToko()
+        {
+            int menu;
+            Console.WriteLine("Menu :");
+            Console.WriteLine();
+            Console.WriteLine("1. Tambah Toko");
+            Console.WriteLine("2. Daftar Toko");
+            Console.WriteLine("3. Kembali ke Menu awal");
+            Console.WriteLine("");
+            Console.Write("Pilih Menu 1/2/3 : ");
+            menu = Convert.ToInt32(Console.ReadLine());
+            if (menu == 1)
+            {
+                Console.Clear();
+                new Program().Tampilan();
+                new Customer().TambahToko();
+            }
+            else
+            {
+                Console.WriteLine("Tidak ada menu tersebut");
+            }
+            Console.ReadLine();
+        }
+        public void TambahToko()
+        {
+
+            Console.WriteLine("Tambah :");
+            Console.Write("ID_Toko   : ");
+            int idCustomer = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nama Customer : ");
+            string nama = Convert.ToString(Console.ReadLine());
+            Console.Write("Alamat : ");
+            string alamat = Convert.ToString(Console.ReadLine());
+            Console.Write("Jenis Kelamin (L/P): ");
+            string jenis = Convert.ToString(Console.ReadLine());
+
+            try
+            {
+                SqlConnection con = new SqlConnection("data source=LAPTOP-8MKEQ456; " +
+                 "database=Tugas2PABD; Integrated Security=True; User ID=sa;Password=mentepermaib20");
+                con.Open();
+
+                SqlCommand sql = new SqlCommand("insert into dbo.Customer (ID_Customer,NamaCustomer,Alamat,Jenis_Kelamin) values ('" + idCustomer + "', '" + nama + "', '" + alamat + "', '" + jenis + "')", con);
+                sql.ExecuteNonQuery();
+                Console.WriteLine("Data berhasil ditambahkan");
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Gagal menambahkan data customer");
+                Console.ReadKey();
+            }
+            Console.WriteLine("1. Kembali ke Menu awal");
+            int kembali = Convert.ToInt32(Console.ReadLine());
+            if (kembali == 1)
+            {
+                Console.Clear();
+                new Program().Tampilan();
+                new Customer().DisplayCustomer();
+            }
+
+
+        }
+    }
+}
